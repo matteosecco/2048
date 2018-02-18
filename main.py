b25893b6ddc04294f76afdf6807bae407d6fe1a8
matteosecco <36543260+matteosecco@users.noreply.github.com>
2018-02-18 15:10:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from random import randint as ri
+from copy import copy
 
 import cell
 
-WIDTH = 800
-LEFT, RIGHT, DOWN, UP = 0, 1, 2, 3
+WIDTH = 400
 
 
 class App:
@@ -12,16 +12,21 @@ class App:
 
         self.root = parent
 
+        # Array where all the cells are saved
+        self.table = [0] * 16
+        # Boolean to control user inputs to avoid too many clicks
+        self._canclick = True
+        # Score
+        self._score = 0
+
         # Draws all the tkinter elements
         self.main_canvas = Canvas(self.root, width=WIDTH, height=WIDTH, bg="lightblue")
         self.main_canvas.pack()
         self.second_frame = Frame(self.root)
         self.second_frame.pack()
-
-        # Array where all the cells are saved
-        self.table = [0] * 16
-        # Boolean to control user inputs to avoid too many clicks
-        self._canclick = True
+        self._scorevar = StringVar()
+        self._scorevar.set(self._score)
+        self._sframesetup()
 
         # Draws the horizontal and vertical lines
         self._griddraw()
@@ -33,15 +38,47 @@ class App:
         """ Handles the user input
 
             direction: LEFT, RIGHT, DOWN, UP = 0, 1, 2, 3"""
-
         if self._canclick:
             self._canclick = False  # Blocks the user input
+
+            # Makes a copy of the table to check later if something changed or not
+            backup = copy(self.table)
 
             d = dict(enumerate([self._left, self._right, self._down, self._up]))
             d[direction]()  # Calls the right movement function
 
-            # Waits until the cells stop and spawns a new one
-            self.root.after(cell.WAIT * 350, self._spawnnew)
+            # Check if there is space to spawn a new cell
+            if not 0 in self.table:
+                self._lose()
+                return
+
+            if backup != self.table:
+                # Waits until the cells stop and spawns a new one
+                self.root.after(301, self._spawnnew)
+            else:
+                self._canclick = True
+
+    def restart(self):
+        """ Restart button callback """
+
+        # deletes lose text
+        self.main_canvas.delete("d72819d9")
+
+        # deletes all cells
+        self.table = [0] * 16
+
+        self._cellsetup(3)
+        self._scorevar.set(0)
+
+    def _sframesetup(self):
+        pointframe = Frame(self.second_frame)
+        pointframe.pack(side=LEFT, pady=20, padx=20)
+
+        Label(pointframe, text="Punteggio:").pack(side=LEFT)
+        Label(pointframe, textvariable=self._scorevar).pack(side=LEFT)
+
+        restartb = Button(self.second_frame, text="Restart", command=self.restart)
+        restartb.pack(side=RIGHT, pady=20, padx=20)
 
     def _griddraw(self):
         """ Draws the horizontal and vertical lines """
@@ -60,6 +97,11 @@ class App:
 
         for _ in range(nstart):
             self._spawnnew()
+
+    def _lose(self):
+        """ Function called when the user is not able to continue the game """
+
+        self.main_canvas.create_text(WIDTH / 2, WIDTH / 2, text="GAME OVER", font=("Helvetica", 25), tag="d72819d9")
 
     def _spawnnew(self):
         """ Creates a new cell and draws it in an empty space """
@@ -89,8 +131,9 @@ class App:
                 while (idx + 1) % 4:  # Keeps going till it reaches the left side
 
                     # 1° CASE: Two cells add up
-                    if isinstance(self.table[idx + 1], cell.Cell) and self.table[idx + 1].n == self.table[idx].n:
+                    if self.table[idx + 1] and self.table[idx + 1].n == self.table[idx].n:
                         self.table[idx + 1].double()  # Doubles a cell
+                        self._scorevar.set(int(self._scorevar.get()) + self.table[idx + 1].n)  # Updates the score label
                         self.table[idx] = 0  # Deletes the other
                         idx += 1
                         break
@@ -120,8 +163,9 @@ class App:
                 c = self.table[idx]
                 while idx % 4:
 
-                    if isinstance(self.table[idx - 1], cell.Cell) and self.table[idx].n == self.table[idx - 1].n:
+                    if self.table[idx - 1] and self.table[idx].n == self.table[idx - 1].n:
                         self.table[idx - 1].double()
+                        self._scorevar.set(int(self._scorevar.get()) + self.table[idx - 1].n)
                         self.table[idx] = 0
                         idx -= 1
                         break
@@ -148,8 +192,9 @@ class App:
                 c = self.table[idx]
                 while not 12 <= idx <= 15:
 
-                    if isinstance(self.table[idx + 4], cell.Cell) and self.table[idx + 4].n == self.table[idx].n:
+                    if self.table[idx + 4] and self.table[idx + 4].n == self.table[idx].n:
                         self.table[idx + 4].double()
+                        self._scorevar.set(int(self._scorevar.get()) + self.table[idx + 4].n)
                         self.table[idx] = 0
                         idx += 4
                         break
@@ -176,8 +221,9 @@ class App:
                 c = self.table[idx]
                 while not 0 <= idx <= 3:
 
-                    if isinstance(self.table[idx - 4], cell.Cell) and self.table[idx - 4].n == self.table[idx].n:
+                    if self.table[idx - 4] and self.table[idx - 4].n == self.table[idx].n:
                         self.table[idx - 4].double()
+                        self._scorevar.set(int(self._scorevar.get()) + self.table[idx - 4].n)
                         self.table[idx] = 0
                         idx -= 4
                         break
@@ -199,9 +245,9 @@ root = Tk()
 
 app = App(root)
 
-root.bind("<d>", lambda event: app.callback(LEFT))
-root.bind("<a>", lambda event: app.callback(RIGHT))
-root.bind("<w>", lambda event: app.callback(UP))
-root.bind("<s>", lambda event: app.callback(DOWN))
+root.bind("<d>", lambda event: app.callback(0))
+root.bind("<a>", lambda event: app.callback(1))
+root.bind("<w>", lambda event: app.callback(3))
+root.bind("<s>", lambda event: app.callback(2))
 
 root.mainloop()
