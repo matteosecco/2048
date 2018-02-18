791533291e0a944de998b379d38fbf3700150868
matteosecco <36543260+matteosecco@users.noreply.github.com>
2018-02-18 15:09:55 +0100

    Update cell.py

--- cell.py
+++ cell.py
@@ -1,6 +1,11 @@
 from uuid import uuid4
+from collections import defaultdict
 
 WAIT = 1  # ms between frames
+
+COLOR_TABLE = defaultdict(lambda: "#ffffff",
+                          **{"2": "#f4e242", "4": "#f4b841", "8": "#f49d41", "16": "#f48241",
+                             "32": "#f46741", "64": "#f45541", "128": "#f44141", "256": "#f4416d"})
 
 
 def sign(n):
@@ -23,9 +28,6 @@ class Cell:
         self.font = ("Helvetica", int(self.l / 3))
         self.id = str(uuid4())  # Personal id of every cell
 
-        self.pxtomove =
-        print(self.pxtomove)
-
         self._draw()
 
     def move(self, x, y):
@@ -39,20 +41,21 @@ class Cell:
     def double(self):
         self.n *= 2
         self.canvas.itemconfig(self.id + "text", text=self.n)
+        self.canvas.itemconfig(self.id + "cell", fill=COLOR_TABLE[str(self.n)])
 
     def _draw(self):
         """ Draws the cell and his number on the canvas"""
 
-        cell_color = "yellow"
         x, y = self.pos
 
-        self.canvas.create_rectangle(x * self.l, y * self.l, (x + 1) * self.l, (y + 1) * self.l, fill=cell_color, tag=self.id)
+        self.canvas.create_rectangle(x * self.l, y * self.l, (x + 1) * self.l, (y + 1) * self.l, fill=COLOR_TABLE[str(self.n)],
+                                     tag=(self.id, self.id + "cell"))
         self.canvas.create_text(x * self.l + (self.l / 2), y * self.l + (self.l / 2), text=self.n, font=self.font, tag=(self.id, self.id + "text"))
 
     def _moveloop(self, tomovex, tomovey):
         """ Recursive function that moves the cell 1px each call """
 
-        if tomovex == 0 and tomovey == 0:
+        if not tomovex and not tomovey:
             return  # Break the loop
 
         self.canvas.move(self.id, sign(tomovex), sign(tomovey))
@@ -65,7 +68,9 @@ class Cell:
         """ When the cell is overwritten his canvas elements must be deleted """
 
         self.canvas.tag_lower(self.id)
-        self.root.after(self.l * 4, lambda: self.canvas.delete(self.id))
+        self.root.after(int(self.l * 4), lambda: self.canvas.delete(self.id))
 
     def __repr__(self):
-        return "%s" % self.n
+        """ Debug purpose """
+
+        return "C(%s)" % self.n
